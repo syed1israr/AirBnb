5d8fcb97290307eab9da4182f402813bb6b57aa8
syedFaraz <syedfaraz173@gmail.com>
2024-01-21 16:20:06 +0530

    Debugging

--- Routes/Wishlist.route.js
+++ Routes/Wishlist.route.js
@@ -5,7 +5,7 @@ const VerifyJWT=require("../Middleware/VerifyJWT.middleware.js")
 const wishlistContoller=require('../Controllers/WishList_Handler.js');
 const {getWishList,postWishlist,deleteWishList}=wishlistContoller
 
-router.route('/').post(VerifyJWT,postWishlist);
-router.route("/:id").delete(VerifyJWT,deleteWishList)
-router.route("/").get(VerifyJWT,getWishList)
+router.route('/').post(postWishlist);
+router.route("/:id").delete(deleteWishList)
+router.route("/").get(getWishList)
 module.exports = router;
